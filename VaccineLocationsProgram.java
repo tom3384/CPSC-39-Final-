4e535b045141da9fc2006c75e15d58a4d03b140a
tom3384 <t_nguyen117@u.pacific.edu>
2024-11-30 06:37:31 +0000

    Getters and setters

---
+++ VaccineLocationsProgram.java
@@ -0,0 +1,6 @@
+// Tom Nguyen
+// 11/29/24
+// CPSC 39 project
+
+// provider_location_guid	loc_store_no	loc_phone	loc_name	loc_admin_street1	loc_admin_street2	loc_admin_city	loc_admin_state	loc_admin_zip	sunday_hours	monday_hours	tuesday_hours	wednesday_hours	thursday_hours	friday_hours	saturday_hours	web_address	pre_screen	insurance_accepted	walkins_accepted	provider_notes	ndc	med_name	in_stock	supply_level	quantity_last_updated	latitude	longitude	Category	Unnamed Column	offers_free_masks	min_age_months	min_age_years	bridge_access_program
+
